96155e5e27f75adab7e521aa8e1bbc3bae768f16
Angelica Mapeso <angelica.mapeso@gmail.com>
2021-09-23 19:06:15 -0400

    change meme displayed

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -12,7 +12,7 @@ async function displayMeme() {
 
     console.log(jsonResponse);
 
-    const meme = jsonResponse.data.memes[1];
+    const meme = jsonResponse.data.memes[2];
 
     const image = document.createElement("img");
     image.src = meme.url;
